108d327f7f7dc22e0b72424cbb3efddb3caf875c
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-12-07 09:31:54 -0800

    Úpdated for AirBnB_clone_V2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
-"""
-script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers.
-"""
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env, put, run
 
-from fabric.api import run, put, env
-import os
-
-env.hosts = ['34.229.66.177', '34.204.101.218']
-env.user = 'ubuntu'
-env.key_filename = '/root/.ssh/school/id_rsa'
+env.hosts = ["34.229.66.177", "34.204.101.218"]
 
 def do_deploy(archive_path):
-    if not os.path.exists(archive_path):
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if not os.path.isfile(archive_path):
         return False
 
-    filename = os.path.basename(archive_path)
-    name_no_ext = filename.split('.')[0]
+    file = os.path.basename(archive_path)
+    name = os.path.splitext(file)[0]
 
-    try:
-        put(archive_path, '/tmp/{}'.format(filename))
-        run('mkdir -p /data/web_static/releases/{}/'.format(name_no_ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, name_no_ext))
-        run('rm /tmp/{}'.format(filename))
-
-        # Merge directories recursively to avoid overwriting or conflicting with existing files
-        run('rsync -av --ignore-existing /data/web_static/releases/{}/web_static/ /data/web_static/releases/{}/'.format(name_no_ext, name_no_ext))
-
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(name_no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name_no_ext))
-
-        return True
-
-    except Exception as e:
+    if put(archive_path, f"/tmp/{file}").failed:
+        return False
+    if run(f"rm -rf /data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"mkdir -p /data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"rm /tmp/{file}").failed:
+        return False
+    if run(f"mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed:
+        return False
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+    if run(f"ln -s /data/web_static/releases/{name}/ /data/web_static/current").failed:
         return False
 
+    return True
