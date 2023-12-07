f0522f45545461796be5d2c25edbb724a3796d99
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-12-07 09:49:23 -0800

    send pack web

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""
-Creates a compressed archive of the web_static folder
-"""
-
-from fabric.api import local
+# A Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
+from fabric.api import local
+
 
 def do_pack():
-    try:
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(now)
-        local("tar -cvzf {} web_static".format(filename))
-        return filename
-    except Exception as e:
+    """To  Create a tar gzipped archive of the directory web_static."""
+    js = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(js.year,
+                                                         js.month,
+                                                         js.day,
+                                                         js.hour,
+                                                         js.minute,
+                                                         js.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-
+    return file
