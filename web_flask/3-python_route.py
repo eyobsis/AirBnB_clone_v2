981bedafd62d20791c20be3f111e8f1412a1b280
eyobsis <ehtube2020@gmail.com>
2024-01-01 16:31:57 -0800

    pycodestyle task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,15 +23,16 @@ def hbnb():
 
 @app.route('/c/<text>')
 def c_route(text):
-    """Route that displays 'C' followed by the value of the text variable"""
+    """ displays 'C' followed by the value of the text variable"""
     return 'C {}'.format(escape(text.replace('_', ' ')))
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_route(text="is cool"):
-    """Route that displays 'Python' followed by the value of the text variable"""
+    """displays 'Python' followed by the value of the text variable"""
     return 'Python {}'.format(escape(text.replace('_', ' ')))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
